3fc9d7cf0e7bedba91d6607554f697cf231943ae
linfeng-931 <s111219030@stu.ntue.edu.tw>
2025-03-07 14:57:01 +0800

    first

---
+++ Desktop/113-2_w2/index.js
@@ -0,0 +1,3 @@
+import { add, mul } from "./test";
+add(1, 3);
+mul(2, 3);
---
+++ Desktop/113-2_w2/test.js
@@ -0,0 +1,4 @@
+const add = (a, b) => console.log("add = " + (a + b));
+const mul = (a, b) => console.log("mul = " + a * b);
+
+export { add, mul };
